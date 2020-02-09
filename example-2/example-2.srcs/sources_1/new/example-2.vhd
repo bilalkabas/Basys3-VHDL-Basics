50fdee53f96fc46ef9c75fc01597953a496dcdef
bilalkabas <bilalkabas@icloud.com>
2020-02-10 01:32:41 +0300

    Example 2 added

---
+++ example-2/example-2.srcs/sources_1/new/example-2.vhd
@@ -0,0 +1,36 @@
+----------------------------------------------------------------------------------
+-- Company: Abdullah Gul University
+-- Engineer: Bilal KABAS
+-- 
+-- Create Date: 07.02.2020 22:49:47
+-- Module Name: Example 2 - Behavioral
+-- Project Name: Example 2 Keyword
+-- Target Devices: Basys3
+-- Revision 0.01 - File Created
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity process_keyword is
+    Generic (
+        LED_NUM : integer:= 16);
+      
+      Port (
+        switch    : in std_logic;
+        btnC      : in std_logic;
+        led       : out std_logic_vector(LED_NUM - 1 downto 0));
+end process_keyword;
+
+architecture Behavioral of process_keyword is
+begin
+    process (switch, btnC)
+    begin
+    if (switch = '1') XOR (btnC = '1') then
+        led <= "1111111111111111";
+    else
+        led <= "0000000000000000";
+    end if;
+    end process;
+end Behavioral;
