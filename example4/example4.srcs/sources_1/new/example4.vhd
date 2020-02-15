3f0d883ff09422774ead39c690e17bdaeb42d4f0
bilalkabas <bilalkabas@icloud.com>
2020-02-16 02:23:55 +0300

    Update example4.vhd

--- example4/example4.srcs/sources_1/new/example4.vhd
+++ example4/example4.srcs/sources_1/new/example4.vhd
@@ -13,7 +13,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity example4 is
-    Generic ( speed : integer := 5 );
+    Generic ( speed : integer := 20 );
     Port    ( clk   : in std_logic;
               led   : buffer std_logic_vector(15 downto 0) := "0000000000000001" );
 end example4;
